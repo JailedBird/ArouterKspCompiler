8ad7131bdd28314e72bc8dd1eb8f5f14efb8c322
JailedBird <113177053+JailedBird@users.noreply.github.com>
2023-04-06 15:02:35 +0800

    feat: Add ARouter function in module app

--- app/src/main/java/cn/jailedbird/arouter/ksp/MainActivity.kt
+++ app/src/main/java/cn/jailedbird/arouter/ksp/MainActivity.kt
@@ -1,9 +1,19 @@
 package cn.jailedbird.arouter.ksp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.alibaba.android.arouter.facade.annotation.Autowired
 
 class MainActivity : AppCompatActivity() {
+
+    @Autowired
+    var list = mutableListOf<String>()
+    @Autowired
+    var arrayList: ArrayList<String> = arrayListOf<String>()
+
+    @Autowired
+    var array: LinkedHashMap<String, ArrayList<Int>>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
