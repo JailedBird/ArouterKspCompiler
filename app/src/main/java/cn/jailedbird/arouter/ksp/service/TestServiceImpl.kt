c46d3b5247353a42bf2aa84a7d47fa4271b3c972
985892345 <guo985892345@foxmail.com>
2023-05-28 17:06:35 +0800

    fix: multiple interface implementation providers are invalid

---
+++ app/src/main/java/cn/jailedbird/arouter/ksp/service/TestServiceImpl.kt
@@ -0,0 +1,16 @@
+package cn.jailedbird.arouter.ksp.service
+
+import android.content.Context
+import com.alibaba.android.arouter.facade.annotation.Route
+
+/**
+ * .
+ *
+ * @author 985892345
+ * 2023/5/28 16:58
+ */
+@Route(path = "/test/service")
+class TestServiceImpl : ITestService1, ITestService2 {
+  override fun init(context: Context) {
+  }
+}
