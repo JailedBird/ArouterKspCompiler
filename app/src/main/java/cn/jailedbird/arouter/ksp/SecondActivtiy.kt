3d59e615d64dff76e447f50eb3a6a76be18f2112
JailedBird <113177053+JailedBird@users.noreply.github.com>
2023-09-12 16:44:20 +0800

    fix: lateinit String crash when Autowired injectd

--- app/src/main/java/cn/jailedbird/arouter/ksp/SecondActivtiy.kt
+++ app/src/main/java/cn/jailedbird/arouter/ksp/SecondActivtiy.kt
@@ -9,24 +9,48 @@ import com.alibaba.android.arouter.launcher.ARouter
 
 @Route(path = "/app/SecondActivtiy")
 class SecondActivtiy : AppCompatActivity() {
+
+    @Autowired
+    var str: String = ""
+    @Autowired
+    var strNull: String? = null
+
+    @Autowired
+    lateinit var lateStr: String
+
     @Autowired
     var list = mutableListOf<String>()
 
+    @Autowired
+    lateinit var lateList: MutableList<String>
+
     @Autowired(required = true)
     var arrayList = arrayListOf<String>()
+
+    @Autowired(required = true)
+    lateinit var lateArrayList: ArrayList<String>
 
     @Autowired
     var array = LinkedHashMap<String, ArrayList<Int>>()
 
     @Autowired
+    lateinit var lateArray: LinkedHashMap<String, ArrayList<Int>>
+
+
+    @Autowired
     var hasSet = HashSet<LinkedHashMap<String, ArrayList<Int>>>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ARouter.getInstance().inject(this)
         setContentView(R.layout.second_main)
         Log.d("TAG", arrayList.toString())
+        Log.d("TAG", lateArrayList.toString())
         Log.d("TAG", list.toString())
+        Log.d("TAG", lateList.toString())
+        Log.d("TAG", str)
+        Log.d("TAG", lateStr)
 
     }
 }
