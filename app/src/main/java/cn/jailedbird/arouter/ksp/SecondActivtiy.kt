9ce587088532ba013c33d527f40f02ad7f396239
JailedBird <113177053+JailedBird@users.noreply.github.com>
2023-10-20 16:13:49 +0800

    fix: IProvider inject error

--- app/src/main/java/cn/jailedbird/arouter/ksp/SecondActivtiy.kt
+++ app/src/main/java/cn/jailedbird/arouter/ksp/SecondActivtiy.kt
@@ -3,20 +3,47 @@ package cn.jailedbird.arouter.ksp
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import cn.jailedbird.arouter.ksp.service.TestServiceImpl
+import cn.jailedbird.arouter.ksp.service.TestServiceImplGene
 import com.alibaba.android.arouter.facade.annotation.Autowired
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
 
 @Route(path = "/app/SecondActivtiy")
-class SecondActivtiy : AppCompatActivity() {
+class SecondActivtiy<T> : AppCompatActivity() {
 
     @Autowired
     var str: String = ""
+
     @Autowired
     var strNull: String? = null
 
     @Autowired
     lateinit var lateStr: String
+
+    @Autowired
+    lateinit var iProvider1: TestServiceImpl
+
+    @Autowired
+    var iProvider2: TestServiceImpl? = null
+
+    @Autowired
+    lateinit var iProvider3: TestServiceImplGene<Int>
+
+    @Autowired
+    var iProvider4: TestServiceImplGene<Int>? = null
+
+    @Autowired(name = "iProvider5")
+    lateinit var iProvider5: TestServiceImpl
+
+    @Autowired(name = "iProvider6")
+    var iProvider6: TestServiceImpl? = null
+
+    @Autowired(name = "iProvider7")
+    lateinit var iProvider7: TestServiceImplGene<Int>
+
+    @Autowired(name = "iProvider8")
+    var iProvider8: TestServiceImplGene<Int>? = null
 
     @Autowired
     var list = mutableListOf<String>()
